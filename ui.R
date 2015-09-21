24cd3dade9231b0b802f5e03d32228a0da284bfb
c5sire <rsimon64@gmail.com>
2015-09-21 13:07:41 -0500

    Enable only necessary

--- ui.R
+++ ui.R
@@ -35,32 +35,33 @@ dashboardPage(skin = "yellow",
           selected = TRUE)
       ),
       menuItem("Phenotype",
-        menuSubItem("Phenotype Dashboard", tabName = "dashboard_phenotype", icon = icon("dashboard"),
-                 selected = TRUE),
-        menuSubItem("New fieldbook", icon = icon("file"), tabName = "fbDesign"),
-        menuSubItem("Import fieldbook", icon = icon("file-excel-o"), tabName = "fbImport"),
-
-        menuSubItem("Check fieldbook", icon = icon("check-square"), tabName = "fbCheck"),
-        menuSubItem("Export fieldbook", icon = icon("file-excel-o"), tabName = "fbExport"),
-        menuSubItem("Import images", icon = icon("photo"), tabName = "fbFotoImport"),
+        # menuSubItem("Phenotype Dashboard", tabName = "dashboard_phenotype", icon = icon("dashboard"),
+        #          selected = TRUE),
+        # menuSubItem("New fieldbook", icon = icon("file"), tabName = "fbDesign"),
+        # menuSubItem("Import fieldbook", icon = icon("file-excel-o"), tabName = "fbImport"),
+        #
+        # menuSubItem("Check fieldbook", icon = icon("check-square"), tabName = "fbCheck"),
+        # menuSubItem("Export fieldbook", icon = icon("file-excel-o"), tabName = "fbExport"),
+        # menuSubItem("Import images", icon = icon("photo"), tabName = "fbFotoImport"),
         menuSubItem("Analysis", icon = icon("book"), tabName = "fieldbook_analysis",
-                    selected = TRUE),
-        menuSubItem("Catalogues", icon = icon("book"), tabName = "fbCatalog")
+                    selected = TRUE)
+        # ,
+        # menuSubItem("Catalogues", icon = icon("book"), tabName = "fbCatalog")
       ),
-      menuItem("Genotype",
-       menuSubItem("Genotype Dashboard", tabName = "dashboard_genotype", icon = icon("dashboard"))
+      # menuItem("Genotype",
+      #  menuSubItem("Genotype Dashboard", tabName = "dashboard_genotype", icon = icon("dashboard"))
       # ,
       #  conditionalPanel(
       #    "input.menu == 'dashboard_genotype'",
       #    selectInput("period", "Period", 1:10)
       #  )
-      ),
+      #),
       menuItem("Environment",
        menuSubItem("Environment Dashboard", tabName = "dashboard_environment", icon = icon("dashboard"))
       ),
-      menuItem("Breeding program",
-       menuSubItem("Program Dashboard", tabName = "dashboard_program", icon = icon("dashboard"))
-      ),
+      # menuItem("Breeding program",
+      #  menuSubItem("Program Dashboard", tabName = "dashboard_program", icon = icon("dashboard"))
+      # ),
       menuItem("Resources",
        menuSubItem("Resources Dashboard", icon = icon("dashboard"), tabName = "resource_dashboard"
                 #selected = TRUE
@@ -77,17 +78,20 @@ dashboardPage(skin = "yellow",
         # )
        # ,
        menuSubItem("Sites", icon = icon("location-arrow"), tabName = "resource_site"),
-       menuSubItem("Data dictionary", icon = icon("book"), tabName = "resource_dictionary"),
-       menuSubItem("Data module", icon = icon("book"), tabName = "resource_module")
+       menuSubItem("Data dictionary", icon = icon("book"), tabName = "resource_dictionary")
+       # ,
+       # menuSubItem("Data module", icon = icon("book"), tabName = "resource_module")
 
-       ),
-    menuItem( "Sharing",
-      menuSubItem("Sharing Dashboard", tabName = "dashboard_sharing", icon = icon("dashboard"))
-    ),
-    menuItem("Help",
-       menuSubItem("Documentation", tabName = "dashboard_help", icon = icon("dashboard")),
-       menuSubItem("Tasks", icon = icon("th"), tabName = "widgets2")
-    )
+       )
+    #   ,
+    # menuItem( "Sharing",
+    #   menuSubItem("Sharing Dashboard", tabName = "dashboard_sharing", icon = icon("dashboard"))
+    # )
+    # ,
+    # menuItem("Help",
+    #    menuSubItem("Documentation", tabName = "dashboard_help", icon = icon("dashboard")),
+    #    menuSubItem("Tasks", icon = icon("th"), tabName = "widgets2")
+    # )
     )
 
   ),
