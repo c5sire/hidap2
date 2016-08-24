93b868f0e0bc5c5a385c04564805c5e252ed5a58
Frojast <rsimon64@gmail.com>
2016-08-24 14:01:07 -0500

    u UI

--- inst/hidap/app.R
+++ inst/hidap/app.R
@@ -161,58 +161,7 @@ ui <- dashboardPage(skin = "yellow",
                                 br()
                         ),
                         tabItem(tabName = "env_dashboard",
-                                fluidRow(
-                                  column(width = 8
-                                         ,
-                                         tabBox(width = NULL, id = "tabLocation",
-                                                tabPanel("Map",
-                                                         leafletOutput("mapLocs")
-                                                )
-                                                ,
-                                                tabPanel("Report",
-                                                         htmlOutput("rep_loc")
-                                                         #HTML("<h1>Under development!</h1>")
-                                                )
-                                         )
-                                  )
-                                  ,
-                                  column(width = 4,
-                                         tabBox(width = NULL, title = "Site"
-                                                ,
-                                                tabPanel("Histogram",
-                                                         plotOutput("histogram")
-                                                )
-                                                ,
-                                                tabPanel("Info",
-                                                         htmlOutput("siteInfo")
-                                                )
-                                                ,
-                                                tabPanel("Fieldtrials",
-                                                         htmlOutput("site_fieldtrials")
-                                                )
-                                                # TODOD
-                                                ,
-                                                tabPanel("Genotypes",
-                                                         htmlOutput("site_genotypes")
-                                                )
-
-                                         )
-                                  )
-                                ),
-
-
-                                fluidRow(
-                                  column(width = 8
-                                         ,
-                                         box(width = NULL,
-                                             title = "Location table"
-                                             ,
-                                             #p(class = 'text-center', downloadButton('locsDL', 'Download Filtered Data')),
-                                             DT::dataTableOutput("tableLocs")
-                                             #locationsUI("location")
-                                         )
-                                  )
-                                )
+                                brapps::locations_ui("Trial Location Explorer")
                         ),
                         tabItem(tabName = "phe_fb_new",
                                 fluidRow((
