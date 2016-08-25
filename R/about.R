61f55a898d706577b4b798813fb1639b06d76c03
Frojast <rsimon64@gmail.com>
2016-08-25 14:58:09 -0500

    u beta

--- R/about.R
+++ R/about.R
@@ -6,7 +6,7 @@
 #' @import shiny
 #' @export
 about <- function(title = "About"){
-  hidap_release = paste0("HIDAP ", packageVersion("hidap"), " [", Sys.Date(), "]")
+  hidap_release = paste0("HIDAP ", packageVersion("hidap"), " beta [", Sys.Date(), "]")
 
   # about_sv <- function(input, output, session) ({
   #   eventReactive(input$about_update, {
@@ -20,7 +20,7 @@ about <- function(title = "About"){
   img(src="about.png", width = "100%"),
   hidap_release,
   p(class = "text-muted", style="text-align:justify",
-    paste("HIDAP is a tool designed to help breeders of clonal plants (likw potato and sweetpotato) carry out field trial planning, documentation, analysis and reporting.")
+    paste("HIDAP is a tool designed to help breeders of clonal crop (like potato and sweetpotato) to carry out field trial planning, documentation, analysis and reporting.")
   ),
   fluidRow(
     column(width = 2, img(src = "Logo1.png", width = "230") ),
