4d4090e0ee757bc75a4b0ebcd35bb7efe4077f96
omarbenites <omar_cs_7@hotmail.com>
2017-07-07 16:37:04 -0500

    new menu items

--- inst/hidap/app.R
+++ inst/hidap/app.R
@@ -89,22 +89,22 @@ ui <- dashboardPage(
                               menuItem("Material Management",
                                        menuSubItem("Manage list", tabName = "manageList", icon = icon("table")),
                                        menuSubItem("Clone list", tabName = "generateList", icon = icon("list")),
-                                       menuSubItem("Family list", tabName = "createList", icon = icon("list-alt"))#,
-                                       #menuSubItem("Parental list", tabName = "parentList", icon = icon("list-alt")),
-                                       #menuSubItem("Distribution Data", tabName = "distributionDB", icon = icon("database"))
+                                       menuSubItem("Family list", tabName = "createList", icon = icon("list-alt")),
+                                       menuSubItem("Parental list", tabName = "parentList", icon = icon("list-alt")),
+                                       menuSubItem("Distribution Data", tabName = "distributionDB", icon = icon("database"))
                               ),
 
                               menuItem("Fieldbook management",
                                        menuSubItem("New fieldbook", tabName = "newFieldbook", icon = icon("file")),
                                        menuSubItem("Open fieldbook", tabName = "openFieldbook", icon = icon("file-o")),
                                        menuSubItem("Check fieldbook", tabName = "checkFieldbook", icon = icon("eraser")),
-                                       menuSubItem("Data Transformation", tabName = "singleAnalysisTrans", icon = icon("file-text-o"))
+                                       menuSubItem("Data transformation", tabName = "singleAnalysisTrans", icon = icon("file-text-o"))
                               ),
 
                               menuItem("Single Trial Analysis",
                                        #menuSubItem("Single trial graph",tabName = "SingleChart", icon = icon("calculator")),
-                                       menuSubItem("Single report", tabName = "singleAnalysisReport", icon = icon("file-text-o")),
-                                       menuSubItem("Data Transformation", tabName = "singleAnalysisTrans", icon = icon("file-text-o"))
+                                       menuSubItem("Single report", tabName = "singleAnalysisReport", icon = icon("file-text-o"))#,
+                                       #menuSubItem("Data Transformation", tabName = "singleAnalysisTrans", icon = icon("file-text-o"))
                               ),
 
                               menuItem("PVS Trial Analysis",
@@ -166,7 +166,7 @@ ui <- dashboardPage(
               br(),
               br(),
 
-              h3("HIDAP v.1.0"),
+              h3("HIDAP v.1.0.1"),
               p(class = "text-muted", style="text-align:justify",
                 #paste("HiDAP is a Highly Interactive Data Analysis Platform originally meant to support clonal crop breeders at the <a href='http://www.cipotato.org' target='_new'>International Potato Center</a>. It is part of a continuous institutional effort to improve data collection, data quality, data analysis and open access publication. The recent iteration simultaneously also represents efforts to unify best practices from experiences in breeding data management of over 10 years, specifically with DataCollector and CloneSelector for potato and sweetpotato breeding, to address new demands for open access publishing and continue to improve integration with both corporate and community databases (such as biomart and sweetpotatobase) and platforms such as the <a href='https://research.cip.cgiar.org/gtdms/' target='_new'> Global Trial Data Management System (GTDMS)</a> at CIP. </br> One of the main new characteristics of the current software development platform established over the last two years is the web-based interface which provides also a highly interactive environment. It could be used both online and offline and on desktop as well as tablets and laptops. Key features include support for data capture, creation of field books, upload field books from and to accudatalogger, data access from breeding databases (e.g., <a href = 'http://germplasmdb.cip.cgiar.org/' target='_new'>CIP BioMart</a>, <a href='http://www.sweetpotatobase.org' target='_new'>sweetpotatobase</a> via <a href='http://docs.brapi.apiary.io/' target='_new'>breeding API</a>), data quality checks, single and multi-environmental data analysis, selection indices, and report generations. For users of DataCollector or CloneSelector many of the features are known but have been improved upon. Novel features include list management of breeding families, connection with the institutional pedigree database, interactive and linked graphs as well as reproducible reports. With the first full release by end of November 2016 we will include all characteristics from both DataCollector and CloneSelector. HIDAP, with additional support from <a href='https://sweetpotatogenomics.cals.ncsu.edu/' target='_new'>GT4SP</a>, <a href='http://www.rtb.cgiar.org/' target='_new'>RTB</a>, USAID, and <a href='http://cipotato.org/research/partnerships-and-special-projects/sasha-program/' target='_new'>SASHA</a>, is aimed to support the broader research community working on all aspects with primary focus on breeding, genetics, biotechnology, physiology and agronomy.")
                 shiny::includeHTML("www/about_hidap.txt")
@@ -327,8 +327,8 @@ sv <- function(input, output, session) ({
   fbmlist::server_managerlist(input, output, session, values)
   fbmlist::server_generate(input, output, session, values)
   fbmlist::server_createlist(input, output, session, values)
-  #fbmlist::server_parentlist(input, output, session, values)
-  #fbmlist::server_distribution(input,output,session, values)
+  fbmlist::server_parentlist(input, output, session, values)
+  fbmlist::server_distribution(input,output,session, values)
 
   fbdesign::server_design(input, output, session, values)
   fbdesign::server_design_big(input, output, session, values)
